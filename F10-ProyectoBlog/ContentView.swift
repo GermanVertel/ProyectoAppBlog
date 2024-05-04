bf264b198a2d2e5558bb0a4889c7b1f44169a952
german vertel <gvertel88@gmail.com>
2024-05-04 10:35:35 -0500

    Initial Commit

---
+++ F10-ProyectoBlog/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  F10-ProyectoBlog
+//
+//  Created by German David Vertel Narvaez on 4/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List(articles){ article in
+            
+            ArticleRow(article : article)
+                .listRowSeparator(.hidden)
+            
+        }
+        .listStyle(.plain)
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct ArticleRow: View {
+    var article: Article
+    
+    var body: some View{
+        VStack{
+            Image(article.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(5)
+            Text(article.title)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+                .lineLimit(3)
+                .padding(.bottom, 0)
+            
+            Text("Creado por \(article.author)".uppercased())
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .padding(.bottom, 0)
+            
+            Text("\(article.url)".lowercased())
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .padding(.bottom, 0)
+            
+            HStack(spacing: 3) {
+                ForEach(1...(article.rating), id: \.self){ _ in
+                    Image(systemName: "star.fill")
+                        .font(.caption)
+                        .foregroundColor(.yellow)
+                }
+                
+            }
+            
+            Text(article.excerpt)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+        }
+    }
+}
---
+++ F10-ProyectoBlog/F10_ProyectoBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  F10_ProyectoBlogApp.swift
+//  F10-ProyectoBlog
+//
+//  Created by German David Vertel Narvaez on 4/05/24.
+//
+
+import SwiftUI
+
+@main
+struct F10_ProyectoBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
