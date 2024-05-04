d9668fb98b478039578f27d309aa21bbe88ee424
german vertel <gvertel88@gmail.com>
2024-05-04 12:52:36 -0500

    creacion de la vista ArticleDetailsView donde mostraremos el articulo completo

---
+++ ArticleDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ArticleDetailView.swift
+//  F10-ProyectoBlog
+//
+//  Created by German David Vertel Narvaez on 4/05/24.
+//
+
+import SwiftUI
+
+struct ArticleDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArticleDetailView()
+}
