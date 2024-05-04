f6662da4e0df001148a04fc17d3bf545c0319412
german vertel <gvertel88@gmail.com>
2024-05-04 13:06:12 -0500

    modificacion de la vistaarticulo

--- ArticleDetailView.swift
+++ ArticleDetailView.swift
@@ -8,11 +8,59 @@
 import SwiftUI
 
 struct ArticleDetailView: View {
+    
+    var article: Article
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+            VStack(alignment: .leading, spacing: 8 ){
+                Image(article.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(5)
+                
+                Group {
+                    Text(article.title)
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(.black)
+                        .multilineTextAlignment(.center)
+                        .lineLimit(3)
+                        .padding(.bottom, 0)
+                    
+                    Text("Creado por \(article.author)".uppercased())
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                        .padding(.bottom, 0)
+                    
+                    Text("\(article.url)".lowercased())
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                        .padding(.bottom, 0)
+                    
+                    
+                }
+                .padding(.bottom, 0)
+                .padding(.horizontal, 16)
+                
+                
+                Group{
+                    Text("\(article.excerpt)")
+                        .bold()
+                    Text(article.content)
+                        
+                }
+                .font(.body)
+                .padding()
+                .lineLimit(100)
+                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                
+            }
+            
+        }
     }
 }
 
 #Preview {
-    ArticleDetailView()
+    ArticleDetailView(article: articles[0])
 }
